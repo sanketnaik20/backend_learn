0df6ef062e153d9c551d7f0886eb09ff49282da6
sanketnaik20 <sanketnaik2004@gmail.com>
2024-04-14 02:25:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/google',(req,res)=>{
+    res.send("sanketnaik")
+})
+
+app.get('/login',(req,res)=>{
+    res.send("<h1>Please login at Sanket</h1>")
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
